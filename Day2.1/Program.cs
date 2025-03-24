43b9efc87cb611ec7828081a5b9fc20e9cec7601
GustavNordgren95 <112472606+GustavNordgren95@users.noreply.github.com>
2025-03-24 21:31:05 +0100

    Maybe backwards but could work

--- Day2.1/Program.cs
+++ Day2.1/Program.cs
@@ -10,35 +10,32 @@ namespace Day2._1
     {
         static void Main(string[] args)
         {
-            // Testing logic to find safe reports
+            // Testing logic to find safe reports, idea is to find the nr of unsafe reports,
+            // subtract the total nr of reports with the nr of unsafe reports and the sum will be the nr of safe reports
+
             int[] testInputs = { 73, 75, 78, 81, 80 };
-            int nrSafeReports = 0;
+            int nrUnsafeReports = 0;
             bool isUnsafe = false;
 
-                for (int i = 0; i < testInputs.Length - 1; i++)
+            for (int i = 0; i < testInputs.Length - 1; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Current element: " + i);
+
+                if (Math.Abs(testInputs[i] - testInputs[i + 1]) <= 2)
                 {
                     Console.WriteLine();
-                    Console.WriteLine("Current element: " + i);
-
-                    if (Math.Abs(testInputs[i] - testInputs[i + 1]) <= 2)
-                    {
-                        Console.WriteLine();
-                        Console.WriteLine("Safe");
-                    }
-                    else
-                    {
-                        isUnsafe = true;
-                        Console.WriteLine();
-                        Console.WriteLine("Unsafe");
-                        break;
-
-                    }
-
-                    if (!isUnsafe)
-                    {
-                        nrSafeReports++;
-                    }
+                    Console.WriteLine("Safe");
                 }
+                else
+                {
+                    isUnsafe = true;
+                    nrUnsafeReports++;
+                    Console.WriteLine();
+                    Console.WriteLine("Unsafe");
+                    break;
+                }
+            }
         }
     }
 }
